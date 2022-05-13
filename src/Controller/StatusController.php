192db1610edf63b23227ba58556d65ef8d8770c6
Lucián Blažek <lucian@ftmo.com>
2022-05-13 21:55:28 +0200

    fix(project): init assets, add status endpoint

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ClownMeister\BohemiaApi\Controller;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class StatusController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    /**
+     * @Route("/status")
+     */
+    public function index(): Response
+    {
+        try {
+            $mysql = $this->entityManager->getConnection()->connect();
+        } catch (Exception $e) {
+            $mysql = false;
+        }
+
+        return new Response(
+            json_encode([
+                'mysql' => $mysql
+            ]),
+            Response::HTTP_OK,
+            [self::HEADER_CONTENT_TYPE => self::CONTENT_TYPE_APPLICATION_JSON]
+        );
+    }
+}
